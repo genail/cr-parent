40b1d06972146af55be6ad3c290761c2f0ef0351
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2009-05-09 16:47:47 +0200

    * Finished create-project.rb script

---
+++ scripts/common.rb
@@ -0,0 +1,89 @@
+require 'ftools'
+
+
+def cp(from, to)
+    notice("copying file from '#{from}' to '#{to}'");
+    File.copy(from, to)
+end
+
+def cp_template(from, to, replace = nil)
+    notice("copying template file from '#{from}' to '#{to}'");
+
+    from_file = File.new(from)
+    from_contents = from_file.read()
+    from_file.close()
+    
+    if (replace != nil)
+        replace.each_key do |key|
+            from_contents.gsub!(key, replace[key]);
+        end
+    end
+    
+    to_file = File.new(to, "a")
+    to_file.write(from_contents)
+    to_file.close()    
+end
+
+def error(message)
+    puts "Error:"
+    puts "\t#{message}"
+    exit 1
+end
+
+def exec(command)
+    puts "# exec: #{command}"
+    system(command)
+end
+
+def notice(message)
+    puts "# #{message}"
+end
+
+def mkdir(dirname)
+    notice("creating directory #{dirname}")
+    Dir.mkdir(dirname)
+end
+
+def prepare_config()
+    if (not File.exists? "scripts/config.rb")
+        notice "Config file not found. Creating one from template."
+        notice "The new config file should be ignored by GIT"
+        cp("scripts/config.rb.template", "scripts/config.rb")
+    end
+end
+
+def prompt(question, answer_regex, default_answer=nil)
+    print "#{question}: "
+    answer = gets.chop()
+    
+    if (answer.empty? and default_answer != nil)
+        answer = default_answer
+    end
+    
+    if (answer =~ answer_regex)
+        return answer
+    else
+        return prompt(question, answer_regex, default_answer)
+    end
+end
+
+
+def require_app(app_name)
+    result = `#{app_name}`
+    
+    if (result.empty?)
+        error("required application '#{app_name}' not found");
+    end
+end
+
+def require_env_variable(name)
+    if (not ENV.has_key? name)
+        error("required enviorment variable '#{name}' not set");
+    end
+end
+
+def require_file(filename)
+    if (not File.exists? filename)
+        error("required file '#{filename}' does not exist")
+    end
+end
---
+++ scripts/create-project.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+
+if (!File.exists? "scripts")
+    puts "This script must be called from CoralReef parent directory"
+    exit 1
+end
+
+require 'scripts/common'
+
+# make sure if config file is available
+# if not then create one from template
+prepare_config();
+
+require 'scripts/config'
+
+notice "New Coral Reef / GIT project creation script"
+
+name = nil
+long_name = nil
+version = nil
+dependencies = nil
+
+begin
+
+    name = prompt("Library name (eg. cr-network)", /^[^ ]+$/);
+    long_name = prompt("Library long name (eg. Network library)", /.+/);
+    version = prompt("Library version [0.1-SNAPSHOT]", /.+/, "0.1-SNAPSHOT")
+    dependencies = prompt("Library dependencies (for README)", /.*/);
+
+    puts
+    puts "Summary:"
+    puts "Library name: #{name}"
+    puts "Library long name: #{long_name}"
+    puts "Library version: #{version}"
+    print "Library dependencies: "
+    
+    if (dependencies.empty?)
+        puts "none"
+    else
+        puts dependencies
+    end
+    
+    puts
+
+    isok = prompt("Is that ok? [yes/no]", /^(yes)|(no)$/)
+end while (isok == "no")
+
+require_env_variable "EDITOR"
+
+require_file "pom.xml.template"
+require_file "gitignore.template"
+require_file "README.template"
+
+require_app "git"
+
+if (File.exists? name)
+    error("cannot create directory '#{name}': file already exists")
+end
+
+replace = {}
+replace["[[name]]"] = name
+replace["[[long_name]]"] = long_name
+replace["[[version]]"] = version
+replace["[[dependencies]]"] = dependencies
+
+
+mkdir name
+cp_template "pom.xml.template", "#{name}/pom.xml", replace
+cp_template "gitignore.template", "#{name}/.gitignore"
+cp_template "README.template", "#{name}/README", replace
+
+mkdir "#{name}/src"
+mkdir "#{name}/src/main"
+mkdir "#{name}/src/main/java"
+mkdir "#{name}/src/main/resources"
+mkdir "#{name}/src/test"
+mkdir "#{name}/src/test/java"
+mkdir "#{name}/src/test/resources"
+
+exec "cd #{name}; git init"
+
+puts
+create_public_repo = prompt("Project created. Do you also want to create a public GIT repository? [yes/no]", /^(yes)|(no)$/)
+
+if (create_public_repo == "yes")
+
+    public_repo = "#{CONFIG::PUBLIC_REPO_DIR}/#{name}.git"
+
+    exec "git clone --bare #{name} #{public_repo}"
+    exec "cd #{name}; git remote add origin #{public_repo}"
+    exec "cd #{public_repo}; git --bare update-server-info; mv hooks/post-update.sample hooks/post-update"
+    
+    puts
+    notice "Your public repo has been successfully created!"
+    notice "It's location is #{CONFIG::PUBLIC_REPO_URL}/#{name}.git"
+end
+
+open_parent_pom = prompt("You should add new project to parent pom.xml. Do you want to do this now? [yes/no]", /^(yes)|(no)$/)
+
+if (open_parent_pom == "yes")
+    system("#{ENV["EDITOR"]} pom.xml")
+end
+
+notice "All done :-)"
